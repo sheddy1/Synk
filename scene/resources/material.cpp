a2eac7322588067775b603e239b60c73ec207008
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-09-03 17:15:37 +0200

    Improve BaseMaterial3D refraction quality by using bicubic filtering
    Performance cost varies depending on how
    much of the viewport is covered by the refractive material.
    This has a moderate performance impact if the refractive material
    covers the whole viewport, but the performance cost is low in
    most other situations.
    
    This works in Forward+ and Mobile, and could work in Compatibility
    when `SCREEN_TEXTURE` reading is implemented there.

--- scene/resources/material.cpp
+++ scene/resources/material.cpp
@@ -968,6 +968,68 @@ void BaseMaterial3D::_update_shader() {
 
 	code += "\n\n";
 
+	code += R"(
+// w0, w1, w2, and w3 are the four cubic B-spline basis functions
+float w0(float a) {
+	return (1.0f / 6.0f) * (a * (a * (-a + 3.0f) - 3.0f) + 1.0f);
+}
+
+float w1(float a) {
+	return (1.0f / 6.0f) * (a * a * (3.0f * a - 6.0f) + 4.0f);
+}
+
+float w2(float a) {
+	return (1.0f / 6.0f) * (a * (a * (-3.0f * a + 3.0f) + 3.0f) + 1.0f);
+}
+
+float w3(float a) {
+	return (1.0f / 6.0f) * (a * a * a);
+}
+
+// g0 and g1 are the two amplitude functions
+float g0(float a) {
+	return w0(a) + w1(a);
+}
+
+float g1(float a) {
+	return w2(a) + w3(a);
+}
+
+// h0 and h1 are the two offset functions
+float h0(float a) {
+	return -1.0f + w1(a) / (w0(a) + w1(a));
+}
+
+float h1(float a) {
+	return 1.0f + w3(a) / (w2(a) + w3(a));
+}
+
+vec4 texture2D_bicubic(sampler2D tex, vec2 uv, int p_lod, ivec2 tex_size) {
+	vec2 tex_size_lod = vec2(tex_size >> p_lod);
+	vec2 pixel_size = vec2(1.0f) / tex_size_lod;
+
+	uv = uv * tex_size_lod + vec2(0.5f);
+
+	vec2 iuv = floor(uv);
+	vec2 fuv = fract(uv);
+
+	float g0x = g0(fuv.x);
+	float g1x = g1(fuv.x);
+	float h0x = h0(fuv.x);
+	float h1x = h1(fuv.x);
+	float h0y = h0(fuv.y);
+	float h1y = h1(fuv.y);
+
+	vec2 p0 = (vec2(iuv.x + h0x, iuv.y + h0y) - vec2(0.5f)) * pixel_size;
+	vec2 p1 = (vec2(iuv.x + h1x, iuv.y + h0y) - vec2(0.5f)) * pixel_size;
+	vec2 p2 = (vec2(iuv.x + h0x, iuv.y + h1y) - vec2(0.5f)) * pixel_size;
+	vec2 p3 = (vec2(iuv.x + h1x, iuv.y + h1y) - vec2(0.5f)) * pixel_size;
+
+	return (g0(fuv.y) * (g0x * textureLod(tex, p0, float(p_lod)) + g1x * textureLod(tex, p1, float(p_lod)))) +
+		   (g1(fuv.y) * (g0x * textureLod(tex, p2, float(p_lod)) + g1x * textureLod(tex, p3, float(p_lod))));
+}
+	)";
+
 	code += "void vertex() {\n";
 
 	if (flags[FLAG_SRGB_VERTEX_COLOR]) {
@@ -1344,7 +1406,9 @@ void BaseMaterial3D::_update_shader() {
 			code += "	vec2 ref_ofs = SCREEN_UV - ref_normal.xy * dot(texture(texture_refraction,base_uv),refraction_texture_channel) * refraction;\n";
 		}
 		code += "	float ref_amount = 1.0 - albedo.a * albedo_tex.a;\n";
-		code += "	EMISSION += textureLod(screen_texture,ref_ofs,ROUGHNESS * 8.0).rgb * ref_amount * EXPOSURE;\n";
+		code += "   vec3 lod_lower = texture2D_bicubic(screen_texture, ref_ofs, int(ROUGHNESS * 8.0), textureSize(screen_texture, 0)).rgb;\n";
+		code += "   vec3 lod_higher = texture2D_bicubic(screen_texture, ref_ofs, int(ROUGHNESS * 8.0) + 1, textureSize(screen_texture, 0)).rgb;\n";
+		code += "	EMISSION += mix(lod_lower, lod_higher, fract(ROUGHNESS * 8.0)) * ref_amount * EXPOSURE;\n";
 		code += "	ALBEDO *= 1.0 - ref_amount;\n";
 		code += "	ALPHA = 1.0;\n";
 
