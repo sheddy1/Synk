2b6ca6f3a392aa58528b0b3cef49210ded2db6ea
kobewi <kobewi4e@gmail.com>
2022-07-31 23:07:10 +0200

    Refactor toggling script list

--- editor/code_editor.h
+++ editor/code_editor.h
@@ -155,6 +155,7 @@ class CodeTextEditor : public VBoxContainer {
 	HBoxContainer *status_bar = nullptr;
 
 	Button *toggle_scripts_button = nullptr;
+	Control *toggle_scripts_list = nullptr;
 	Button *error_button = nullptr;
 	Button *warning_button = nullptr;
 
@@ -279,6 +280,7 @@ public:
 
 	void validate_script();
 
+	void set_toggle_list_control(Control *p_control);
 	void show_toggle_scripts_button();
 	void update_toggle_scripts_button();
 
