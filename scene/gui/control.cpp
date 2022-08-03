eaf86c3d8bf2b73e7a3e31027804e6dc942607da
Konkitoman <konkitoman@users.noreply.github.com>
2022-08-03 17:10:03 +0300

    Fix: when editor was in single window, other embedded windows when open a popup was not where they should

--- scene/gui/control.cpp
+++ scene/gui/control.cpp
@@ -1319,10 +1319,10 @@ Point2 Control::get_global_position() const {
 Point2 Control::get_screen_position() const {
 	ERR_FAIL_COND_V(!is_inside_tree(), Point2());
 	Point2 global_pos = get_global_transform_with_canvas().get_origin();
-	//Window *w = Object::cast_to<Window>(get_viewport());
-	//if (w && !w->is_embedding_subwindows()) {
-	//	global_pos += w->get_position();
-	//}
+	Window *w = Object::cast_to<Window>(get_viewport());
+	if (w && w->should_be_embedded()) {
+		global_pos += w->get_position();
+	}
 
 	return global_pos;
 }
@@ -1386,10 +1386,10 @@ Rect2 Control::get_screen_rect() const {
 
 	Rect2 r(get_global_position(), get_size());
 
-	//Window *w = Object::cast_to<Window>(get_viewport());
-	//if (w && !w->is_embedding_subwindows()) { 
-	//	r.position += w->get_position();
-	//}
+	Window *w = Object::cast_to<Window>(get_viewport());
+	if (w && w->should_be_embedded()) { 
+		r.position += w->get_position();
+	}
 
 	return r;
 }
