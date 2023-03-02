b7af17b403222ef0ad5440531eca5bd824596b02
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-03-01 19:58:12 -0800

    Add exrwav export to Godot Engine for movie making.

--- servers/register_server_types.cpp
+++ servers/register_server_types.cpp
@@ -58,6 +58,7 @@
 #include "debugger/servers_debugger.h"
 #include "display_server.h"
 #include "movie_writer/movie_writer.h"
+#include "movie_writer/movie_writer_exrwav.h"
 #include "movie_writer/movie_writer_mjpeg.h"
 #include "movie_writer/movie_writer_pngwav.h"
 #include "navigation_server_2d.h"
@@ -113,6 +114,7 @@ static bool has_server_feature_callback(const String &p_feature) {
 
 static MovieWriterMJPEG *writer_mjpeg = nullptr;
 static MovieWriterPNGWAV *writer_pngwav = nullptr;
+static MovieWriterEXRWAV *writer_exrwav = nullptr;
 
 void register_server_types() {
 	shader_types = memnew(ShaderTypes);
@@ -286,6 +288,9 @@ void register_server_types() {
 
 	writer_pngwav = memnew(MovieWriterPNGWAV);
 	MovieWriter::add_writer(writer_pngwav);
+
+	writer_exrwav = memnew(MovieWriterEXRWAV);
+	MovieWriter::add_writer(writer_exrwav);
 }
 
 void unregister_server_types() {
@@ -293,6 +298,7 @@ void unregister_server_types() {
 	memdelete(shader_types);
 	memdelete(writer_mjpeg);
 	memdelete(writer_pngwav);
+	memdelete(writer_exrwav);
 }
 
 void register_server_singletons() {
