fc77882a7dd673c36e7207e7287db1e09a262305
ywmaa <ywmaa.personal@gmail.com>
2023-10-15 05:17:20 +0300

    Enable use of global value force_vertex_lighting

--- drivers/gles3/storage/material_storage.cpp
+++ drivers/gles3/storage/material_storage.cpp
@@ -1187,7 +1187,6 @@ MaterialStorage::MaterialStorage() {
 
 		actions.render_mode_defines["skip_vertex_transform"] = "#define SKIP_TRANSFORM_USED\n";
 		actions.render_mode_defines["unshaded"] = "#define MODE_UNSHADED\n";
-		actions.render_mode_defines["vertex_lighting"] = "#define USE_VERTEX_LIGHTING\n";
 		actions.render_mode_defines["light_only"] = "#define MODE_LIGHT_ONLY\n";
 		actions.render_mode_defines["world_vertex_coords"] = "#define USE_WORLD_VERTEX_COORDS\n";
 
@@ -2862,6 +2861,7 @@ void SceneShaderData::set_code(const String &p_code) {
 	wireframe = false;
 
 	unshaded = false;
+	vertex_lighting = false;
 	uses_vertex = false;
 	uses_position = false;
 	uses_sss = false;
@@ -2924,6 +2924,7 @@ void SceneShaderData::set_code(const String &p_code) {
 	actions.render_mode_values["cull_back"] = Pair<int *, int>(&cull_modei, CULL_BACK);
 
 	actions.render_mode_flags["unshaded"] = &unshaded;
+	actions.render_mode_flags["vertex_lighting"] = &vertex_lighting;
 	actions.render_mode_flags["wireframe"] = &wireframe;
 	actions.render_mode_flags["particle_trails"] = &uses_particle_trails;
 	actions.render_mode_flags["world_vertex_coords"] = &uses_world_coordinates;
