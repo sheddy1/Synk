8965d9810355ad7b002b67705257c563b4435d5b
Jakub Marcowski <chubercikbattle@gmail.com>
2024-04-16 01:37:48 +0200

    Update `ucaps.h` to contain proper case matchings (Python parity)

--- core/string/ucaps_fetch.py
+++ core/string/ucaps_fetch.py
@@ -1,68 +1,28 @@
 """
-Script used to fetch the latest `CaseFolding.txt` file
-from the Unicode Character Database (UCD) and parse it
-to extract case mappings for Unicode characters.
+Script used to dump case mappings
+from the Unicode Character Database
+to the `ucaps.h` file.
 """
 
-import urllib.request
-from typing import List, Tuple
 import os
+from typing import List, Tuple
+
+lower_to_upper: List[Tuple[str, str]] = []
+for i in range(int("FFFF", 16)):
+    char = chr(i)
+    if char.islower():
+        lower_to_upper.append((f"0x{i:04X}", f"0x{ord(char.upper()[0]):04X}"))
+ltu_len = len(lower_to_upper)
+
+upper_to_lower: List[Tuple[str, str]] = []
+for i in range(int("FFFF", 16)):
+    char = chr(i)
+    if char.isupper():
+        upper_to_lower.append((f"0x{i:04X}", f"0x{ord(char.lower()[0]):04X}"))
+utl_len = len(upper_to_lower)
 
 
-def txt_fetch(target_url: str) -> List[str]:
-    """
-    Fetches a text file from the given URL and returns it as a list of lines.
-
-    :param target_url: The URL of the text file to fetch.
-    :return: A list of lines from the fetched text file.
-    """
-    return [line.decode("utf-8") for line in urllib.request.urlopen(target_url)]
-
-
-# CaseFolding-15.1.0.txt
-# Date: 2023-05-12, 21:53:10 GMT
-# © 2023 Unicode®, Inc.
-#
-# Format:
-# <code>; <status>; <mapping>; # <name>
-URL = "https://www.unicode.org/Public/UCD/latest/ucd/CaseFolding.txt"
-
-lines = txt_fetch(URL)
-lines = lines[62 : (len(lines) - 2)]  # Trim unnecessary data.
-
-
-class CaseMapping:
-    def __init__(self, code: str, status: str, mapping: str, name: str) -> None:
-        self.code = code
-        self.status = status
-        self.mapping = mapping
-        self.name = name
-
-    def __str__(self) -> str:
-        return f"{self.code}; {self.status}; {self.mapping}  # {self.name}"
-
-
-case_mappings: List[CaseMapping] = []
-
-for line in lines:
-    split_line = line.split(";")
-    case_mapping = CaseMapping(
-        split_line[0].strip(),
-        split_line[1].strip(),
-        split_line[2].strip(),
-        split_line[3].strip()[2:],
-    )
-    case_mappings.append(case_mapping)
-
-case_mappings_parsed: List[Tuple[List[str], str]] = [
-    (case_mapping.mapping.split(), case_mapping.code) for case_mapping in case_mappings if len(case_mapping.code) == 4
-]
-
-# Add `Latin Small Letter Reversed E` (ɘ)
-# which is missing for some reason.
-case_mappings_parsed.append((["0258"], "018E"))
-
-ucaps_str = """/**************************************************************************/
+ucaps_str = f"""/**************************************************************************/
 /*  ucaps.h                                                               */
 /**************************************************************************/
 /*                         This file is part of:                          */
@@ -95,49 +55,39 @@ ucaps_str = """/****************************************************************
 #ifndef UCAPS_H
 #define UCAPS_H
 
-static const int caps_table[][2] = {
+#define LTU_LEN {ltu_len}
+#define UTL_LEN {utl_len}
+
+static const int caps_table[LTU_LEN][2] = {{
 \t"""
 
-case_mappings_parsed.sort(key=lambda x: int(x[0][0], 16))
-lower_to_caps_used = set()
-
-for code, mapping in case_mappings_parsed:
-    code_tmp = code[0]
-    if code_tmp in lower_to_caps_used:
-        continue
-    ucaps_str += f"{{ 0x{code_tmp}, 0x{mapping} }},\n\t"
-    lower_to_caps_used.add(code_tmp)
+for lower, upper in lower_to_upper:
+    ucaps_str += f"{{ {lower}, {upper} }},\n\t"
 
 ucaps_str = ucaps_str[:-1]  # Remove trailing tab.
 ucaps_str += """};
 
-static const int reverse_caps_table[][2] = {
+static const int reverse_caps_table[UTL_LEN][2] = {
 \t"""
 
-case_mappings_parsed.sort(key=lambda x: int(x[1], 16))
-caps_to_lower_used = set()
-
-for code, mapping in case_mappings_parsed:
-    if mapping in caps_to_lower_used:
-        continue
-    ucaps_str += f"{{ 0x{mapping}, 0x{code[0]} }},\n\t"
-    caps_to_lower_used.add(mapping)
+for upper, lower in upper_to_lower:
+    ucaps_str += f"{{ {upper}, {lower} }},\n\t"
 
 ucaps_str = ucaps_str[:-1]  # Remove trailing tab.
 ucaps_str += """};
 
 static int _find_upper(int ch) {
 \tint low = 0;
-\tint high = sizeof(caps_table)/sizeof(caps_table[0]) - 1;
+\tint high = LTU_LEN - 1;
 \tint middle;
 
 \twhile (low <= high) {
 \t\tmiddle = (low + high) / 2;
 
 \t\tif (ch < caps_table[middle][0]) {
-\t\t\thigh = middle - 1;  // Search low end of array.
+\t\t\thigh = middle - 1; // Search low end of array.
 \t\t} else if (caps_table[middle][0] < ch) {
-\t\t\tlow = middle + 1;  // Search high end of array.
+\t\t\tlow = middle + 1; // Search high end of array.
 \t\t} else {
 \t\t\treturn caps_table[middle][1];
 \t\t}
@@ -148,16 +98,16 @@ static int _find_upper(int ch) {
 
 static int _find_lower(int ch) {
 \tint low = 0;
-\tint high = sizeof(caps_table)/sizeof(caps_table[0]) - 2;
+\tint high = UTL_LEN - 1;
 \tint middle;
 
 \twhile (low <= high) {
 \t\tmiddle = (low + high) / 2;
 
 \t\tif (ch < reverse_caps_table[middle][0]) {
-\t\t\thigh = middle - 1;  // Search low end of array.
+\t\t\thigh = middle - 1; // Search low end of array.
 \t\t} else if (reverse_caps_table[middle][0] < ch) {
-\t\t\tlow = middle + 1;  // Search high end of array.
+\t\t\tlow = middle + 1; // Search high end of array.
 \t\t} else {
 \t\t\treturn reverse_caps_table[middle][1];
 \t\t}
