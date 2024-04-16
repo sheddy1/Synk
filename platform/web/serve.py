Faless/godot

[Web] Fix browser opening too early with serve.py

platform/web/serve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test  # type: ignore
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pathlib import Path
 import os
 import sys
@@ -27,12 +27,21 @@ def shell_open(url):
 def serve(root, port, run_browser):
     os.chdir(root)
 
+    address = ("", port)
+    httpd = HTTPServer(address, CORSRequestHandler)
+
     if run_browser:
         # Open the served page in the user's default browser.
         print("Opening the served URL in the default browser (use `--no-browser` or `-n` to disable this).")
         shell_open(f"http://127.0.0.1:{port}")
 
-    test(CORSRequestHandler, HTTPServer, port=port)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, stopping server.")
+    finally:
+        # Clean-up server
+        httpd.server_close()
 
 
 if __name__ == "__main__":

sheddy1/Synk

platform/web/serve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test  # type: ignore
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pathlib import Path
 import os
 import sys
@@ -38,12 +38,21 @@ def shell_open(url):
 def serve(root, port, run_browser):
     os.chdir(root)
 
+    address = ("", port)
+    httpd = DualStackServer(address, CORSRequestHandler)
+
     if run_browser:
         # Open the served page in the user's default browser.
         print("Opening the served URL in the default browser (use `--no-browser` or `-n` to disable this).")
         shell_open(f"http://127.0.0.1:{port}")
 
-    test(CORSRequestHandler, DualStackServer, port=port)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, stopping server.")
+    finally:
+        # Clean-up server
+        httpd.server_close()
 
 
 if __name__ == "__main__":