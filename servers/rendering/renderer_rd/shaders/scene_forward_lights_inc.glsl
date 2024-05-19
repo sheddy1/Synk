f44d1d795729f3094a92461021a9c2b313efb112
Eliseu Amaro <mail@eliseuama.ro>
2024-05-19 21:27:48 +0200

    Applied clang-format to modifications

--- servers/rendering/renderer_rd/shaders/scene_forward_lights_inc.glsl
+++ servers/rendering/renderer_rd/shaders/scene_forward_lights_inc.glsl
@@ -2,11 +2,11 @@
 
 /* [Drobot2014a] Low Level Optimizations for GCN. */
 float sqrt_IEEE_int_approximation(float v) {
-    return intBitsToFloat(0x1fbd1df5 + (floatBitsToInt(v) >> 1));
+	return intBitsToFloat(0x1fbd1df5 + (floatBitsToInt(v) >> 1));
 }
 
 vec2 sqrt_IEEE_int_approximation(vec2 v) {
-    return intBitsToFloat(0x1fbd1df5 + (floatBitsToInt(v) >> 1));
+	return intBitsToFloat(0x1fbd1df5 + (floatBitsToInt(v) >> 1));
 }
 
 float D_GGX(float cos_theta_m, float alpha) {
@@ -18,17 +18,17 @@ float D_GGX(float cos_theta_m, float alpha) {
 // From "Course Notes: Moving Frostbite to PBR", page 12: https://media.contentapi.ea.com/content/dam/eacom/frostbite/files/course-notes-moving-frostbite-to-pbr-v32.pdf
 float V_GGX(float NdotL, float NdotV, float alpha) {
 	// Original formulation of G_SmithGGX Correlated
-    // lambda_v = ( -1 + sqrt ( alphaG2 * (1 - NdotL2 ) / NdotL2 + 1) ) * 0.5 f ;
-    // lambda_l = ( -1 + sqrt ( alphaG2 * (1 - NdotV2 ) / NdotV2 + 1) ) * 0.5 f ;
-    // G_ SmithGGXCorrelated = 1 / (1 + lambda_v + lambda_l ) ;
-    // V_ SmithGGXCorrelated = G_SmithGGXCorrelated / (4.0 f * NdotL * NdotV ) ;
+	// lambda_v = ( -1 + sqrt ( alphaG2 * (1 - NdotL2 ) / NdotL2 + 1) ) * 0.5 f ;
+	// lambda_l = ( -1 + sqrt ( alphaG2 * (1 - NdotV2 ) / NdotV2 + 1) ) * 0.5 f ;
+	// G_ SmithGGXCorrelated = 1 / (1 + lambda_v + lambda_l ) ;
+	// V_ SmithGGXCorrelated = G_SmithGGXCorrelated / (4.0 f * NdotL * NdotV ) ;
 
-    // This is the optimized version
-    // Caution : the " NdotL *" and " NdotV *" are explicitely inversed , this is not a mistake.
+	// This is the optimized version
+	// Caution : the " NdotL *" and " NdotV *" are explicitely inversed , this is not a mistake.
 	float Lambda_GGXV = NdotL * sqrt_IEEE_int_approximation((-NdotV * alpha + NdotV) * NdotV + alpha);
 	float Lambda_GGXL = NdotV * sqrt_IEEE_int_approximation((-NdotL * alpha + NdotL) * NdotL + alpha);
 
-	return 0.5 / (Lambda_GGXV + Lambda_GGXL );
+	return 0.5 / (Lambda_GGXV + Lambda_GGXL);
 }
 
 float D_GGX_anisotropic(float cos_theta_m, float alpha_x, float alpha_y, float cos_phi, float sin_phi) {
@@ -43,15 +43,11 @@ float D_GGX_anisotropic(float cos_theta_m, float alpha_x, float alpha_y, float c
 // From "Course Notes: Moving Frostbite to PBR", page 12: https://media.contentapi.ea.com/content/dam/eacom/frostbite/files/course-notes-moving-frostbite-to-pbr-v32.pdf
 // This is a modified version to take anisotropy into account
 float V_GGX_anisotropic(float alpha_x, float alpha_y, float TdotV, float TdotL, float BdotV, float BdotL, float NdotV, float NdotL) {
-    float Lambda_GGXV = NdotL * sqrt_IEEE_int_approximation(
-        (-NdotV * (alpha_x * TdotV * TdotV + alpha_y * BdotV * BdotV) + NdotV) * NdotV
-        + alpha_x * TdotV * TdotV + alpha_y * BdotV * BdotV);
+	float Lambda_GGXV = NdotL * sqrt_IEEE_int_approximation((-NdotV * (alpha_x * TdotV * TdotV + alpha_y * BdotV * BdotV) + NdotV) * NdotV + alpha_x * TdotV * TdotV + alpha_y * BdotV * BdotV);
 
-    float Lambda_GGXL = NdotV * sqrt_IEEE_int_approximation(
-        (-NdotL * (alpha_x * TdotL * TdotL + alpha_y * BdotL * BdotL) + NdotL) * NdotL
-        + alpha_x * TdotL * TdotL + alpha_y * BdotL * BdotL);
+	float Lambda_GGXL = NdotV * sqrt_IEEE_int_approximation((-NdotL * (alpha_x * TdotL * TdotL + alpha_y * BdotL * BdotL) + NdotL) * NdotL + alpha_x * TdotL * TdotL + alpha_y * BdotL * BdotL);
 
-    return 0.5 / (Lambda_GGXV + Lambda_GGXL);
+	return 0.5 / (Lambda_GGXV + Lambda_GGXL);
 }
 
 float SchlickFresnel(float u) {
