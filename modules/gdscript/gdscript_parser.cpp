espumita00/test

Improve autocompletion by analyzing incomplete fragments from the parser

modules/gdscript/gdscript_parser.cpp
@@ -222,6 +222,23 @@ void GDScriptParser::push_warning(const Node *p_source, GDScriptWarning::Code p_
 }
 #endif
 
+inline void GDScriptParser::push_incomplete(GDScriptParser::Node *p_incomplete) {
+#ifdef TOOLS_ENABLED
+	if (p_incomplete != nullptr) {
+		incomplete_fragments.append(p_incomplete);
+	}
+#endif
+}
+
+inline void GDScriptParser::apply_incomplete(GDScriptParser::Node *p_apply_to) {
+#ifdef TOOLS_ENABLED
+	if (p_apply_to != nullptr) {
+		p_apply_to->incomplete_fragments.append_array(incomplete_fragments);
+		incomplete_fragments.clear();
+	}
+#endif
+}
+
 void GDScriptParser::make_completion_context(CompletionType p_type, Node *p_node, int p_argument, bool p_force) {
 	if (!for_completion || (!p_force && completion_context.type != COMPLETION_NONE)) {
 		return;
@@ -988,6 +1005,7 @@ GDScriptParser::VariableNode *GDScriptParser::parse_property(VariableNode *p_var
 	if (p_need_indent) {
 		if (!consume(GDScriptTokenizer::Token::INDENT, R"(Expected indented block for property after ":".)")) {
 			complete_extents(p_variable);
+			push_incomplete(p_variable);
 			return nullptr;
 		}
 	}
@@ -998,6 +1016,7 @@ GDScriptParser::VariableNode *GDScriptParser::parse_property(VariableNode *p_var
 
 	if (!consume(GDScriptTokenizer::Token::IDENTIFIER, R"(Expected "get" or "set" for property declaration.)")) {
 		complete_extents(p_variable);
+		push_incomplete(p_variable);
 		return nullptr;
 	}
 
@@ -1645,6 +1664,9 @@ GDScriptParser::SuiteNode *GDScriptParser::parse_suite(const String &p_context, 
 	if (p_for_lambda) {
 		lambda_ended = true;
 	}
+
+	apply_incomplete(suite);
+
 	current_suite = suite->parent_block;
 	return suite;
 }
@@ -1809,6 +1831,8 @@ GDScriptParser::Node *GDScriptParser::parse_statement() {
 		synchronize();
 	}
 
+	apply_incomplete(result);
+
 	return result;
 }
 
@@ -1833,6 +1857,7 @@ GDScriptParser::AssertNode *GDScriptParser::parse_assert() {
 			push_error(R"(Expected error message for assert after ",".)");
 			pop_multiline();
 			complete_extents(assert);
+			push_incomplete(assert->condition);
 			return nullptr;
 		}
 		match(GDScriptTokenizer::Token::COMMA);
@@ -2014,6 +2039,8 @@ GDScriptParser::MatchNode *GDScriptParser::parse_match() {
 		current_suite->has_return = true;
 	}
 
+	apply_incomplete(match);
+
 	return match;
 }
 
@@ -2078,6 +2105,9 @@ GDScriptParser::MatchBranchNode *GDScriptParser::parse_match_branch() {
 
 	if (!consume(GDScriptTokenizer::Token::COLON, vformat(R"(Expected ":"%s after "match" %s.)", has_guard ? "" : R"( or "when")", has_guard ? "pattern guard" : "patterns"))) {
 		complete_extents(branch);
+		for (GDScriptParser::PatternNode *E : branch->patterns) {
+			push_incomplete(E);
+		}
 		return nullptr;
 	}
 
@@ -2883,12 +2913,16 @@ GDScriptParser::ExpressionNode *GDScriptParser::parse_dictionary(ExpressionNode 
 
 			if (key != nullptr && value != nullptr) {
 				dictionary->elements.push_back({ key, value });
+			} else {
+				push_incomplete(key);
+				push_incomplete(value);
 			}
 		} while (match(GDScriptTokenizer::Token::COMMA) && !is_at_end());
 	}
 	pop_multiline();
 	consume(GDScriptTokenizer::Token::BRACE_CLOSE, R"(Expected closing "}" after dictionary elements.)");
 	complete_extents(dictionary);
+	apply_incomplete(dictionary);
 
 	return dictionary;
 }

sheddy1/Synk

modules/gdscript/gdscript_parser.cpp
@@ -234,6 +234,23 @@ void GDScriptParser::apply_pending_warnings() {
 }
 #endif
 
+inline void GDScriptParser::push_incomplete(GDScriptParser::Node *p_incomplete) {
+#ifdef TOOLS_ENABLED
+	if (p_incomplete != nullptr) {
+		incomplete_fragments.append(p_incomplete);
+	}
+#endif
+}
+
+inline void GDScriptParser::apply_incomplete(GDScriptParser::Node *p_apply_to) {
+#ifdef TOOLS_ENABLED
+	if (p_apply_to != nullptr) {
+		p_apply_to->incomplete_fragments.append_array(incomplete_fragments);
+		incomplete_fragments.clear();
+	}
+#endif
+}
+
 void GDScriptParser::make_completion_context(CompletionType p_type, Node *p_node, int p_argument, bool p_force) {
 	if (!for_completion || (!p_force && completion_context.type != COMPLETION_NONE)) {
 		return;
@@ -1104,6 +1121,7 @@ GDScriptParser::VariableNode *GDScriptParser::parse_property(VariableNode *p_var
 	if (p_need_indent) {
 		if (!consume(GDScriptTokenizer::Token::INDENT, R"(Expected indented block for property after ":".)")) {
 			complete_extents(p_variable);
+			push_incomplete(p_variable);
 			return nullptr;
 		}
 	}
@@ -1114,6 +1132,7 @@ GDScriptParser::VariableNode *GDScriptParser::parse_property(VariableNode *p_var
 
 	if (!consume(GDScriptTokenizer::Token::IDENTIFIER, R"(Expected "get" or "set" for property declaration.)")) {
 		complete_extents(p_variable);
+		push_incomplete(p_variable);
 		return nullptr;
 	}
 
@@ -1748,6 +1767,9 @@ GDScriptParser::SuiteNode *GDScriptParser::parse_suite(const String &p_context, 
 	if (p_for_lambda) {
 		lambda_ended = true;
 	}
+
+	apply_incomplete(suite);
+
 	current_suite = suite->parent_block;
 	return suite;
 }
@@ -1924,6 +1946,8 @@ GDScriptParser::Node *GDScriptParser::parse_statement() {
 		synchronize();
 	}
 
+	apply_incomplete(result);
+
 	return result;
 }
 
@@ -1948,6 +1972,7 @@ GDScriptParser::AssertNode *GDScriptParser::parse_assert() {
 			push_error(R"(Expected error message for assert after ",".)");
 			pop_multiline();
 			complete_extents(assert);
+			push_incomplete(assert->condition);
 			return nullptr;
 		}
 		match(GDScriptTokenizer::Token::COMMA);
@@ -2159,6 +2184,8 @@ GDScriptParser::MatchNode *GDScriptParser::parse_match() {
 	}
 	match_branch_annotation_stack.clear();
 
+	apply_incomplete(match_node);
+
 	return match_node;
 }
 
@@ -2223,6 +2250,9 @@ GDScriptParser::MatchBranchNode *GDScriptParser::parse_match_branch() {
 
 	if (!consume(GDScriptTokenizer::Token::COLON, vformat(R"(Expected ":"%s after "match" %s.)", has_guard ? "" : R"( or "when")", has_guard ? "pattern guard" : "patterns"))) {
 		complete_extents(branch);
+		for (GDScriptParser::PatternNode *E : branch->patterns) {
+			push_incomplete(E);
+		}
 		return nullptr;
 	}
 
@@ -3013,12 +3043,16 @@ GDScriptParser::ExpressionNode *GDScriptParser::parse_dictionary(ExpressionNode 
 
 			if (key != nullptr && value != nullptr) {
 				dictionary->elements.push_back({ key, value });
+			} else {
+				push_incomplete(key);
+				push_incomplete(value);
 			}
 		} while (match(GDScriptTokenizer::Token::COMMA) && !is_at_end());
 	}
 	pop_multiline();
 	consume(GDScriptTokenizer::Token::BRACE_CLOSE, R"(Expected closing "}" after dictionary elements.)");
 	complete_extents(dictionary);
+	apply_incomplete(dictionary);
 
 	return dictionary;
 }